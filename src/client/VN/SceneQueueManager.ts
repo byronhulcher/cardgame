050d9e56b50c0e948d5661fe0b2187d45f4bcf9e
Byron Hulcher <byronhulcher@gmail.com>
2020-11-06 11:57:44 -0500

    Dynamically typed SceneQueueManager, but you probably can't use this

--- src/client/VN/SceneQueueManager.ts
+++ src/client/VN/SceneQueueManager.ts
@@ -3,22 +3,30 @@ import autoBind from 'auto-bind'
 import { LogicActionQueueItem } from './LogicActionQueue'
 import { SceneQueue } from './SceneQueue'
 import { ISceneActions } from './types'
-import { ArgumentsOf } from './utils'
+import {
+  assertIs,
+  ArgumentsOf,
+  HasMethods
+} from './utils'
 
-export class SceneQueueManager {
+class _SceneQueueManager {
   sceneQueue: SceneQueue
 
   constructor(actions: ISceneActions) {
+    assertIs<HasMethods<ISceneActions>>(this)
+
     this.sceneQueue = new SceneQueue(actions)
     const actionKeys = (Object.keys(actions) as Array<keyof typeof actions>)
-
     actionKeys.forEach(
       (action) => {
-        SceneQueueManager.prototype[action] =
-          (...args: ArgumentsOf<ISceneActions[typeof action]>) =>
-            this.sceneQueue.push({ action, args } as LogicActionQueueItem<ISceneActions>)
+        _SceneQueueManager.prototype[action] =
+          (...args: [...ArgumentsOf<ISceneActions[typeof action]>]): void =>
+            this.sceneQueue.push({ action, args, } as LogicActionQueueItem<ISceneActions>)
       })
 
     autoBind(this)
   }
+
 }
+
+export const SceneQueueManager = _SceneQueueManager as _SceneQueueManager & { new(): _SceneQueueManager & HasMethods<ISceneActions> }
--- src/client/VN/utils.ts
+++ src/client/VN/utils.ts
@@ -1,1 +1,8 @@
 export type ArgumentsOf<T> = T extends (...args: infer T) => unknown ? T : never;
+
+export type HasMethods<T> = { [k in keyof T]: (...args: ArgumentsOf<T[k]>) => void }
+
+// General purpose assert function
+// If before this, value had type `U`,
+// afterwards the type will be `U & T`
+export declare function assertIs<T>(value: unknown): asserts value is T
