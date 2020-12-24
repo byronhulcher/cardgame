a20b038d558117c4c025fbda8a433162d1a598f3
Byron Hulcher <byronhulcher@gmail.com>
2020-12-24 06:55:32 -0500

    Linting fixes

--- src/client/CardGame/CardGameLogic.ts
+++ src/client/CardGame/CardGameLogic.ts
@@ -33,79 +33,79 @@ export const CardGameLogic = kea({
   // Here's each of our game states values from `GameState`, mapped to different Actions from `GameActions` which update them deterministicly
   reducers: () => ({
     resourcePoints: [
-[
-  0,
-  0,
-] as ResourcePointsTuple,
-{
-  resetGame: () => [
-    0,
-    0,
-  ] as ResourcePointsTuple,
-  playCard: (currentResourcePoints: ResourcePointsTuple, {
-    player,
-    card,
-  }: { player: Player, card: Card }) =>
-    getCardData(card).updateResourcePoints(currentResourcePoints, player),
-},
+      [
+        0,
+        0,
+      ] as ResourcePointsTuple,
+      {
+        resetGame: () => [
+          0,
+          0,
+        ] as ResourcePointsTuple,
+        playCard: (currentResourcePoints: ResourcePointsTuple, {
+          player,
+          card,
+        }: { player: Player, card: Card }) =>
+          getCardData(card).updateResourcePoints(currentResourcePoints, player),
+      },
     ],
     victoryPoints: [
-[
-  0,
-  0,
-] as VictoryPointsTuple,
-{
-  resetGame: () => [
-    0,
-    0,
-  ] as VictoryPointsTuple,
-  playCard: (currentVictoryPoints: VictoryPointsTuple, {
-    player,
-    card,
-  }: { player: Player, card: Card }): VictoryPointsTuple =>
-    getCardData(card).updateVictoryPoints(currentVictoryPoints, player),
-  harvestResources: (currentVictoryPoints: VictoryPointsTuple, {
-    player,
-    modifier,
-  }: { player: Player, modifier: number }) =>
-    currentVictoryPoints.map((points, index) => index === player ? points + modifier : points),
-},
+      [
+        0,
+        0,
+      ] as VictoryPointsTuple,
+      {
+        resetGame: () => [
+          0,
+          0,
+        ] as VictoryPointsTuple,
+        playCard: (currentVictoryPoints: VictoryPointsTuple, {
+          player,
+          card,
+        }: { player: Player, card: Card }): VictoryPointsTuple =>
+          getCardData(card).updateVictoryPoints(currentVictoryPoints, player),
+        harvestResources: (currentVictoryPoints: VictoryPointsTuple, {
+          player,
+          modifier,
+        }: { player: Player, modifier: number }) =>
+          currentVictoryPoints.map((points, index) => index === player ? points + modifier : points),
+      },
     ],
     hands: [
-[
-  [],
-  [],
-] as HandTuple,
-{
-  playCard: (currentHandTuple: HandTuple, {
-    player,
-    card,
-  }: { player: Player, card: Card }) =>
-    currentHandTuple.map((hand, index) => index === player ? hand.filter((handCard) => handCard !== card) : hand),
-  drawCard: (currentHandTuple: HandTuple, {
-    player,
-    card,
-  }: { player: Player, card: Card }) =>
-    currentHandTuple.map((hand, index) => index === player ? [
-      ...hand,
-      card,
-    ] : hand),
-},
+      [
+        [],
+        [],
+      ] as HandTuple,
+      {
+        playCard: (currentHandTuple: HandTuple, {
+          player,
+          card,
+        }: { player: Player, card: Card }) =>
+          currentHandTuple.map((hand, index) => index === player ? hand.filter((handCard) => handCard !== card) : hand),
+        drawCard: (currentHandTuple: HandTuple, {
+          player,
+          card,
+        }: { player: Player, card: Card }) =>
+          currentHandTuple.map((hand, index) => index === player ? [
+            ...hand,
+            card,
+          ] : hand),
+      },
     ],
     decks: [
-[
-  [
-    Card.Remove2R,
-    Card.Sacrifice1RGain1V,
-    Card.Sacrifice4RGain6R,
-  ],
-  [],
-] as HandTuple,
-{ drawCard: (currentDeckTuple: DeckTuple, {
-  player,
-  card,
-}: { player: Player, card: Card }) =>
-  currentDeckTuple.map((deck, index) => index === player ? deck.filter((deckCard) => deckCard !== card) : deck) },
+      [
+        [
+          Card.Remove2R,
+          Card.Sacrifice1RGain1V,
+          Card.Sacrifice4RGain6R,
+        ],
+        [],
+      ] as HandTuple,
+      { drawCard: (currentDeckTuple: DeckTuple, {
+        player,
+        card,
+      }: { player: Player, card: Card }) =>
+        currentDeckTuple.map((deck, index) => index === player ? deck.filter((deckCard) => deckCard !== card) : deck) },
     ],
     activePlayer: [
       Player.One,
